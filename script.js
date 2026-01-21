b86ba76bdf744d176f319d1b492e1334853cac88
indraputra1540-cpu <indraputra1540@gmail.com>
2026-01-21 19:39:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let produk = JSON.parse(localStorage.getItem("produk")) || [];
+
+
+function tampilkanProduk() {
+const container = document.getElementById("produk");
+container.innerHTML = "";
+
+
+produk.forEach((item, index) => {
+container.innerHTML += `
+<div class="card">
+<img src="${item.gambar}">
+<h3>${item.nama}</h3>
+<p>Rp ${item.harga}</p>
+<button onclick="hapusProduk(${index})">Hapus</button>
+</div>
+`;
+});
+
+
+localStorage.setItem("produk", JSON.stringify(produk));
+}
+
+
+function tambahProduk() {
+const nama = document.getElementById("nama").value;
+const harga = document.getElementById("harga").value;
+const gambar = document.getElementById("gambar").value;
+
+
+if (!nama || !harga || !gambar) {
+alert("Lengkapi semua data!");
+return;
+}
+
+
+produk.push({ nama, harga, gambar });
+tampilkanProduk();
+
+
+document.getElementById("nama").value = "";
+document.getElementById("harga").value = "";
+document.getElementById("gambar").value = "";
+}
+
+
+function hapusProduk(index) {
+produk.splice(index, 1);
+tampilkanProduk();
+}
+
+
+tampilkanProduk();
